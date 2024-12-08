b396019ba80448415910f39a2bc3f29b62875c07
larao <emptemp0000@gmail.com>
2024-12-08 22:43:06 +0900

    root commit

---
+++ src/extract-env.ts
@@ -0,0 +1,36 @@
+/// 必要な環境変数名を可変長引数に渡すと、環境変数ごとに対応した値が入ったオブジェクトを返す。
+///
+/// # 例外
+///
+/// 対応した環境変数が定義されておらず`defaults`にも存在しない場合は例外を送出する。
+///
+/// # 使用例
+///
+/// ```ts
+/// const env = extractEnv(["DISCORD_TOKEN", "MAIN_CHANNEL_ID"]);
+/// ```
+export function extractEnv<K extends string>(
+  keys: readonly K[],
+  defaults?: Readonly<Partial<Record<K, string>>>
+): Record<K, string> {
+  const env: Partial<Record<K, string>> = {};
+  // `K` は可変長引数である配列の値が取りうる型で、キーのリテラル型全ての直和型が得られる。
+  // この直和型を `Record<K, string>` のキーにすればいい感じの戻り値型になる。
+  // {`Record` についてこちらを参照： https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type}
+  // だから、戻り値型は `Record<K, string> になる。`
+  for (const key of keys) {
+    // `keys` から一個ずつ取り出して
+    if (process.env[key] !== undefined && process.env[key] !== "") {
+      // 存在したらそれに設定
+      env[key] = process.env[key];
+    } else if (defaults?.[key] !== undefined) {
+      // `defaults` にフォールバック
+      env[key] = defaults[key];
+    } else {
+      // 存在しなければ例外を送出
+      throw new Error(`env is not set: ${key}`);
+    }
+  }
+  // すでに問題ないことを確認したので `as` で変換
+  return env as Record<K, string>;
+}
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { REST, Routes, Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+import { extractEnv } from "./extract-env";
+
+dotenv.config();
+const { DISCORD_BOT_TOKEN, DISCORD_CLIENT_ID } = extractEnv([
+  "DISCORD_BOT_TOKEN",
+  "DISCORD_CLIENT_ID",
+]);
+
+const commands = [
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(DISCORD_BOT_TOKEN);
+
+try {
+  console.log("Started refreshing application (/) commands.");
+
+  await rest.put(Routes.applicationCommands(DISCORD_CLIENT_ID), {
+    body: commands,
+  });
+
+  console.log("Successfully reloaded application (/) commands.");
+} catch (error) {
+  console.error(error);
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(DISCORD_BOT_TOKEN);
